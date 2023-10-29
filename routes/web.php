104757afe5214d8286448c8f3bf98e86e60ad348
sagabsam <201240001065@unisnu.ac.id>
2023-10-29 16:26:54 +0700

    user list and pagination

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('profile-edit', function(){
         return view('pages.profile', ['type_menu' => '']);
     })->name('profile.edit');
+
+    Route::resource('user', UserController::class);
 });
 
 // Route::get('/login', function () {
